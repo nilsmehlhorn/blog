06b431864c694f7c66cb4b2365686a85e0e884cc
mehlhorn <mail@nils-mehlhorn.de>
2019-09-04 19:12:42 +0200

    Connect mailchimp

--- src/components/seo.js
+++ src/components/seo.js
@@ -65,7 +65,9 @@ function SEO({description, lang, meta, keywords, title}) {
                   : []
               )
               .concat(meta)}
-          />
+          >
+            <script id="mcjs">{`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/b9d12be3afd40fff71025c8d6/a5d7b0b6f8f43c554770eb6c0.js");`}</script>
+          </Helmet>
         )
       }}
     />
