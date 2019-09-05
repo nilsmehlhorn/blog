e34b1321c7442a0a5a591cdf80f70f444570aeb9
mehlhorn <mail@nils-mehlhorn.de>
2019-09-05 23:02:58 +0200

    Fix related posts

--- src/components/related-posts.js
+++ src/components/related-posts.js
@@ -3,13 +3,16 @@ import React from 'react'
 import PostLink from './post-link'
 
 const RelatedPosts = ({posts}) => {
+  if (!posts.length) {
+    return null
+  }
   const selected = []
-  do {
-    const s = posts[Math.floor(Math.random() * posts.length)]
-    if (!selected.includes(s)) {
-      selected.push(s)
+  while (selected.length < Math.min(posts.length, 2)) {
+    const p = posts[Math.floor(Math.random() * posts.length)]
+    if (!selected.includes(p)) {
+      selected.push(p)
     }
-  } while (selected.length < Math.min(posts.length, 2))
+  }
   const related = selected
     .map((node, i) => {
       const post = <PostLink key={node.id + '-related'} post={node}/>
