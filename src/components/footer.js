662174d4931f99899402243e81352716d3eaf1c3
mehlhorn <mail@nils-mehlhorn.de>
2019-09-21 15:58:32 +0200

    Add RSS icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaGithub, FaTwitter, FaInstagram, FaDev} from 'react-icons/fa'
+import {FaGithub, FaTwitter, FaInstagram, FaDev, FaRss} from 'react-icons/fa'
 import {GoMailRead} from 'react-icons/go'
 
 import styles from './footer.module.scss'
@@ -12,6 +12,7 @@ const Footer = () => (
             <a title='Nils Mehlhorn on Instagram' href="https://www.instagram.com/nils_mehlhorn"><FaInstagram size={32}/></a>
             <a title='Nils Mehlhorn on GitHub' href="https://github.com/nilsmehlhorn"><FaGithub size={32}/></a>
             <a title='Nils Mehlhorn on DEV' href="https://dev.to/n_mehlhorn"><FaDev size={32}/></a>
+            <a title='Nils Mehlhorn via RSS' href="https://nils-mehlhorn.de/rss.xml"><FaRss size={32}/></a>
             <button className={styles.link} title='Nils Mehlhorn Newsletter' onClick={() => window.ml_webform_1483080('show')}>
               <GoMailRead size={32}/>
             </button>
