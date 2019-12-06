e95a9ccd45cdd4c329a816dc46e818d69c7bdc95
mehlhorn <mail@nils-mehlhorn.de>
2019-12-06 17:51:54 +0100

    Redesign
    - add slides

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -14,12 +14,12 @@ const TalksPage = ({
                      }
                    }) => {
   const Talks = talks
-    .map(({node: {talk}}) => <div className={styles.talk}>
-      <a href={talk.link}>
+    .map(({node: {talk, id}}) => <div key={id} className={styles.talk}>
+      <a href={talk.slides} target="_blank" rel="noopener noreferrer">
         <Img className={styles.banner} fluid={talk.img.preview.fluid}/>
       </a>
       <div className={styles.content}>
-        <h2 className={styles.name}>{talk.name}</h2>
+        <h2 className={styles.name}><a href={talk.link}>{talk.name}</a></h2>
         <div className={styles.sub}>
           <p className="date">{talk.date}</p>
           <Location>{talk.location}</Location>
@@ -28,7 +28,7 @@ const TalksPage = ({
       </div>
     </div>)
   return <Layout>
-    <SEO key={'seo'} title={'Blog'} keywords={['talk', 'public speaking', 'angular', 'javascript', 'nodejs', 'nativescript']}/>
+    <SEO key={'seo'} title={'Talks'} keywords={['talk', 'public speaking', 'angular', 'javascript', 'nodejs', 'nativescript']}/>
     <div className={classes('content-padding', styles.talks)}>
       {Talks}
     </div>
@@ -48,6 +48,7 @@ export const pageQuery = graphql`
             date(formatString: "MMMM DD, YYYY")
             location
             description
+            slides
             img {
               preview: childImageSharp {
                 fluid(maxHeight: 480, quality: 100) {
