07c660c09333cd8093f9614bfb8a4ab29da85d94
mehlhorn <mail@nils-mehlhorn.de>
2020-06-29 16:15:12 +0200

    Fix RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,12 +108,26 @@ module.exports = {
                       frontmatter {
                         title
                         date
+                        path
+                        description
+                        published
                       }
                     }
                   }
                 }
               }
-        `
+          `,
+          serialize: ({ query: { site, allMarkdownRemark } }) => allMarkdownRemark.edges
+            .filter(edge => edge.node.frontmatter.published)
+            .map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description: edge.node.frontmatter.description || edge.node.excerpt,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+                custom_elements: [{ "content:encoded": edge.node.html }],
+              })
+            })
         }]
       }
     },
